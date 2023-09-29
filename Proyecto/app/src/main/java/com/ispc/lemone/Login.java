f7e577c5893593f05eacc777e67341cbe047fab2
Easy1089 <lumediaweb@gmail.com>
2023-09-29 13:11:07 -0300

    Subida Activity Acerca de y Contacto - Fabiola Benitez

--- Proyecto/app/src/main/java/com/ispc/lemone/Login.java
+++ Proyecto/app/src/main/java/com/ispc/lemone/Login.java
@@ -2,7 +2,9 @@ package com.ispc.lemone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -11,4 +13,6 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
 }
