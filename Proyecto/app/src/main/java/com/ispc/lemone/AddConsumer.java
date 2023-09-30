0b5ba3f1830fb963f76d6b2b5f20400cec8358fa
edguazzetti <edguazzetti@gmail.com>
2023-09-30 11:30:22 -0300

    fix1

--- Proyecto/app/src/main/java/com/ispc/lemone/AddConsumer.java
+++ Proyecto/app/src/main/java/com/ispc/lemone/AddConsumer.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddConsumer extends AppCompatActivity {
@@ -12,6 +13,16 @@ public class AddConsumer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_final_consumer);
+
+        Button Save = findViewById(R.id.btn_eliminar);
+
+        Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddProvider.this, Proveed.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- Proyecto/app/src/main/java/com/ispc/lemone/AddProvider.java
+++ Proyecto/app/src/main/java/com/ispc/lemone/AddProvider.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddProvider extends AppCompatActivity {
@@ -12,6 +13,16 @@ public class AddProvider extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_provider);
+
+         Button Save = findViewById(R.id.buttonGuardarConsumidor);
+
+          Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddProvider.this, Proveed.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
