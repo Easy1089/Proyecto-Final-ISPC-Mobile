d4e24e9bd6a8f2ebc9eeab5171fb338a8a57f284
Andrea-Cal <andiesc83@hotmail.com>
2023-09-30 01:08:39 -0300

    agrego navegabilidad al boton de Agregar Producto en FindProduct.java

--- Proyecto/app/src/main/java/com/ispc/lemone/FindProduct.java
+++ Proyecto/app/src/main/java/com/ispc/lemone/FindProduct.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -12,6 +13,7 @@ public class FindProduct extends AppCompatActivity {
 
     private FrameLayout btnVolver;
     private TextView editar;
+    private Button agregarProducto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class FindProduct extends AppCompatActivity {
 
         btnVolver = findViewById(R.id.btn_volverFP);
         editar = findViewById(R.id.editarFP);
+        agregarProducto = findViewById(R.id.btn_agregarFP);
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,6 +35,14 @@ public class FindProduct extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(FindProduct.this, EditProduct.class);
+                startActivity(intent);
+            }
+        });
+
+        agregarProducto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FindProduct.this, AddProduct.class);
                 startActivity(intent);
             }
         });
