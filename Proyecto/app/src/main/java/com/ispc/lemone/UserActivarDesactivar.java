49846c9159d76fbe7ff940c2eb77b48dcad8a001
Salvador Berón <salvadorberon@gmail.com>
2023-09-30 19:35:40 -0300

    Linkeo de botones en ventana de restablecer clave y activar/desactivar usuario

--- Proyecto/app/src/main/java/com/ispc/lemone/UserActivarDesactivar.java
+++ Proyecto/app/src/main/java/com/ispc/lemone/UserActivarDesactivar.java
@@ -2,13 +2,37 @@ package com.ispc.lemone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserActivarDesactivar extends AppCompatActivity {
+
+    Button btn_user;
+    Button btn_cancel_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadousuario);
+
+        btn_cancel_user=findViewById(R.id.btn_cancelar_user);
+
+        btn_cancel_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserActivarDesactivar.this, ConsultUser.class);
+            }
+        });
+
+        btn_user = findViewById(R.id.act_desact_usuario);
+        btn_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserActivarDesactivar.this, AddUser.class);
+                startActivity(intent);
+            }
+        });
     }
 }
