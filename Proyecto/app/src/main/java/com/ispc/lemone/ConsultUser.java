466bbd73128a5c354843eade5d3b85b89a2738d3
melisaapaz <melisaapaz@gmail.com>
2023-09-30 11:48:43 -0300

    Navegabilidad en card de users

--- Proyecto/app/src/main/java/com/ispc/lemone/ConsultUser.java
+++ Proyecto/app/src/main/java/com/ispc/lemone/ConsultUser.java
@@ -2,13 +2,31 @@ package com.ispc.lemone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 public class ConsultUser extends AppCompatActivity {
+
+    private Button buttonModificar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consult_user);
+
+        buttonModificar = findViewById(R.id.buttonModificar3);
+
+        buttonModificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Botón Modificar usuario", "Clic en el botón modificar usuario");
+                Intent intent = new Intent(ConsultUser.this, EdidUser.class);
+                startActivity(intent);
+            }
+        });
     }
 }
