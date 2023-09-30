b864fad6f7790aacec616c2533261d3b1d629fb7
Andrea-Cal <andiesc83@hotmail.com>
2023-09-30 00:17:09 -0300

    agrego navegabilidad al boton de volver atras y al link de editar en FindProduct.java

--- Proyecto/app/src/main/java/com/ispc/lemone/FindProduct.java
+++ Proyecto/app/src/main/java/com/ispc/lemone/FindProduct.java
@@ -2,13 +2,38 @@ package com.ispc.lemone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class FindProduct extends AppCompatActivity {
+
+    private FrameLayout btnVolver;
+    private TextView editar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_product);
+
+        btnVolver = findViewById(R.id.btn_volverFP);
+        editar = findViewById(R.id.editarFP);
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FindProduct.this, MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FindProduct.this, EditProduct.class);
+                startActivity(intent);
+            }
+        });
     }
 }
